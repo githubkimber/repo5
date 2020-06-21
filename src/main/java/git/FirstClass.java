a0aece919984f91be85db7771d66c70301923ea5
wj <wj@123.com>
2020-06-21 19:35:30 +0800

    修改了git-repo5.IDEA文件.

--- src/main/java/git/FirstClass.java
+++ src/main/java/git/FirstClass.java
@@ -2,11 +2,15 @@ package git;
 
 public class FirstClass {
     public static void main(String[] args) {
+
         System.out.println("hello git project !");
+        // hello test !
+        System.out.println("hello test !");
     }
 
     public void test(){
         int i = 0;
         i += 10 ;
+        System.out.println(i);
     }
 }
